c5ded90075026172e663b8de09884bd372f26f54
Semuel <serg_artemenko@ukr.net>
2023-08-31 13:43:11 +0300

    feat: delete html folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
 import cartIcon from '../images/cart-icon.svg';
 import nerdsLogo from '../images/nerds-logo.svg';
+import Slide1 from '../images/slide_1.png'
+import Slide2 from '../images/slide_2.png'
+import Slide3 from '../images/slide_3.png'
 
 export const Home = () => {
     return ( 
         <>
-            <nav classNameName="navbar navbar-expand-lg pt-50 bg-color-main">
-                <div classNameName="container">
-                    <a classNameName="navbar-brand" href="!#">
-                        <img classNameName="d-inline-block align-text-top" src={nerdsLogo} width="160" heigth="65"  alt="Nerds logo" />
+            <nav className="navbar navbar-expand-lg pt-50 bg-color-main">
+                <div className="container">
+                    <a className="navbar-brand" href="!#">
+                        <img className="d-inline-block align-text-top" src={nerdsLogo} width="160" heigth="65"  alt="Nerds logo" />
                     </a>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
@@ -15,21 +18,21 @@ export const Home = () => {
                     </button>
 
                     <div className="collapse navbar-collapse ms-5" id="navbarSupportedContent">
-                        <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li classNameName="nav-item"><a classNameName="nav-link active" href="!#">Студия</a> </li>
-                            <li classNameName="nav-item"><a classNameName="nav-link" href="!#">Клиенты</a></li>
-                            <li classNameName="nav-item"><a classNameName="nav-link" href="!#">Магазин</a></li>
-                            <li classNameName="nav-item"><a classNameName="nav-link" href="!#">Контакты</a></li> 
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item"><a className="nav-link active" href="!#">Студия</a> </li>
+                            <li className="nav-item"><a className="nav-link" href="!#">Клиенты</a></li>
+                            <li className="nav-item"><a className="nav-link" href="!#">Магазин</a></li>
+                            <li className="nav-item"><a className="nav-link" href="!#">Контакты</a></li> 
                         </ul>
-                        <div classNameName="d-flex gap-3">
+                        <div className="d-flex gap-3">
                             <img src={cartIcon} width="15" heigth="11" alt="Cart"/>
-                            <a classNameName="nav-link" href="!#">Корзина</a>
+                            <a className="nav-link" href="!#">Корзина</a>
                         </div>
                     </div>    
                 </div>
             </nav>
 
-            <div classNameName="container">
+            <div className="container">
                 <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -38,21 +41,21 @@ export const Home = () => {
                     </div>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src={Slide1} className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Метка первого слайда</h5>
                             <p>Некоторый репрезентативный заполнитель для первого слайда.</p>
                         </div>
                         </div>
                         <div className="carousel-item">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src={Slide2} className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Метка второго слайда</h5>
                             <p>Некоторый репрезентативный заполнитель для второго слайда.</p>
                         </div>
                         </div>
                         <div className="carousel-item">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src={Slide3} className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Метка третьего слайда</h5>
                             <p>Некоторый репрезентативный заполнитель для третьего слайда.</p>
