e694c85ef636956d3adccbaf9040da6ae690706e
Semuel <serg_artemenko@ukr.net>
2023-09-01 16:52:50 +0300

    fix: fixed className in Advantage component

--- src/components/Mapcontact.jsx
+++ src/components/Mapcontact.jsx
@@ -1,56 +1,71 @@
+import { useState } from 'react';
+
 export const Mapcontact = () => {
 
+    const [toogleMoodal, setToogleModal] = useState(false);
+
+    const openModalWindow = (event) => {
+        event.preventDefault();
+        setToogleModal(true);
+
+    }
+
     return ( 
-        <div className="container-fluid p-0">
-            <h2 className="visually-hidden">Как нас найти</h2>
-            <div className="wrap-map">
-                <div className="ratio ratio-21x9 p-0" style={{height: '416px'}}>
-                    <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1998.603625300316!2d30.320858716173465!3d59.93871916905408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696310fca145cc1%3A0x42b32648d8238007!2z0JHQvtC70YzRiNCw0Y8g0JrQvtC90Y7RiNC10L3QvdCw0Y8g0YPQuy4sIDE5LzgsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTExODY!5e0!3m2!1sru!2sru!4v1613493564716!5m2!1sru!2sru"
-                    height="416"
-                    style={{border: 0}}
-                    allowfullscreen=""
-                    aria-hidden="false"
-                    tabindex="0"
-                    title="Офис компании по адресу ул. Большая Конюшенная 19/8, Санкт-Петербург"
-                    ></iframe>
-                </div>
-
-                {/* <div className="contacts-inner container">
-                    <div className="contacts-adress">
-                    <p className="adress-title">NЁRDS DESIGN STUDIO</p>
-                    <p className="adress-text">
-                        191186, Санкт-Петербург,<br />
-                        ул. Б. Конюшенная, д. 19/8<br />
-                    </p>
-                    <p className="tel-text">
-                        тел.
-                        <a className="footer-phone" href="tel:+78122757575"
-                        >+7 (812) 275-75-75</a
-                        >
-                    </p>
-                    <a href="writeus.html">Напишите нам</a>
+        <>
+            <div className="container-fluid p-0">
+                <h2 className="visually-hidden">Как нас найти</h2>
+                <div className="wrap-map">
+                    <div className="ratio ratio-21x9 p-0" style={{height: '416px'}}>
+                        <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1998.603625300316!2d30.320858716173465!3d59.93871916905408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696310fca145cc1%3A0x42b32648d8238007!2z0JHQvtC70YzRiNCw0Y8g0JrQvtC90Y7RiNC10L3QvdCw0Y8g0YPQuy4sIDE5LzgsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTExODY!5e0!3m2!1sru!2sru!4v1613493564716!5m2!1sru!2sru"
+                        height="416"
+                        style={{border: 0}}
+                        allowFullScreen=""
+                        aria-hidden="false"
+                        tabIndex="0"
+                        title="Офис компании по адресу ул. Большая Конюшенная 19/8, Санкт-Петербург"
+                        ></iframe>
                     </div>
-                </div> */}
 
-                <div className="modal" tabindex="-1">
-                    <div className="modal-dialog">
-                        <div clasName="modal-content">
-                        <div clasName="modal-header">
-                            <h5 clasName="modal-title">Заголовок модального окна</h5>
-                            <button type="button" clasName="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+                    <div className="contacts-inner container">
+                        <div className="contacts-adress">
+                        <p className="adress-title">NЁRDS DESIGN STUDIO</p>
+                        <p className="adress-text">
+                            191186, Санкт-Петербург,<br />
+                            ул. Б. Конюшенная, д. 19/8<br />
+                        </p>
+                        <p className="tel-text">
+                            тел.
+                            <a className="footer-phone" href="tel:+78122757575"
+                            >+7 (812) 275-75-75</a
+                            >
+                        </p>
+                        <a href="writeus.html">Напишите нам</a>
                         </div>
-                        <div className="modal-body">
-                            <p>Здесь идет основной текст модального окна</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-                            <button type="button" className="btn btn-primary">Сохранить изменения</button>
-                        </div>
-                        </div>
+                        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={openModalWindow}>
+                            Напишите нам
+                        </button>
                     </div>
+
+                   {toogleMoodal && <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Заголовок модального окна</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                                    <button type="button" className="btn btn-primary">Сохранить изменения</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>}
                 </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/components/main/Advantage.jsx
+++ src/components/main/Advantage.jsx
@@ -7,7 +7,7 @@ export const Advantage = () => {
         <div className="container mt-5 d-flex justify-content-between">
             <div className="card" style={{width: '18.75rem', height: '21.87rem'}}>
                 <img src={advantage1} className="card-img-top mb-4" alt="..." height= '146px' width='300px' />
-                <div clasName="card-body">
+                <div className="card-body">
                     <h3 className="card-title mb-3">Веб-сайты</h3>
                     <p className="card-text mb-4">Мир никогда не будет прежним после того как увидит ваш сайт!</p>
                     <a href="!#" className="btn btn-primary">УЗНАТЬ БОЛЬШЕ</a>
@@ -15,7 +15,7 @@ export const Advantage = () => {
             </div>
             <div className="card" style={{width: '18.75rem', height: '21.87rem'}}>
                 <img src={advantage2} className="card-img-top mb-4" alt="..." />
-                <div clasName="card-body">
+                <div className="card-body">
                     <h3 className="card-title mb-3">Приложения</h3>
                     <p className="card-text mb-4">Покорите топ-10 приложений в AppStore и Google Play</p>
                     <a href="!#" className="btn btn-primary">УЗНАТЬ БОЛЬШЕ</a>
@@ -23,7 +23,7 @@ export const Advantage = () => {
             </div>
             <div className="card" style={{width: '18.75rem', height: '21.87rem'}}>
                 <img src={advantage3} className="card-img-top mb-4" alt="..." />
-                <div clasName="card-body">
+                <div className="card-body">
                     <h3 className="card-title mb-3">Презентации</h3>
                     <p className="card-text mb-4">Вы даже не подозреваете, насколько вы изумительны!</p>
                     <a href="!#" className="btn btn-primary">УЗНАТЬ БОЛЬШЕ</a>
