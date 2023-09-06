0b53d677031187857dd1a5530720c0b7bb87bf4e
Semuel <serg_artemenko@ukr.net>
2023-09-06 12:50:02 +0300

    feat: added code out to the CheckboxGroup component

--- src/components/checkbox-group/CheckboxGroup.jsx
+++ src/components/checkbox-group/CheckboxGroup.jsx
@@ -1,5 +1,23 @@
-export const CheckboxGroup = () => {
-    return ( 
-        <h1>Check box</h1>
+export const CheckboxGroup = (props) => {
+
+    const { options } = props;
+
+    return (
+        <div>
+            <h1>Check box</h1>
+            <form>
+                <h3>Особености:</h3>
+
+                {options.map(({ text, value, id }) =>
+
+                    <div key={text} className="form-check">                           
+                        <input className="form-check-input" type="checkbox" value={value} id={id} />
+                        <label className="form-check-label" htmlFor='defaultCheck1'>
+                            {text}
+                        </label>
+                    </div>
+                )}
+            </form>
+        </div> 
     )
 }
