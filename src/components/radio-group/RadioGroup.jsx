c937c12e7cb370baa019f23bbac22d105856f4ee
Semuel <serg_artemenko@ukr.net>
2023-09-06 12:31:46 +0300

    feat: added code out to the RadioGroup component

--- src/components/radio-group/RadioGroup.jsx
+++ src/components/radio-group/RadioGroup.jsx
@@ -1,6 +1,25 @@
-export const RadioGroup = () => {
+export const RadioGroup = (props) => {
+
+    const { options } = props;
+
+    console.log(options)
     return ( 
-        <h1>RadioGroup</h1>
+        <div>
+            <h1>RadioGroup</h1>
+            <form>
+                <h3>Сітка:</h3>
+
+                {options.map(({ text, id }) =>
+
+                    <div key={text} className="form-check" >
+                        <input className="form-check-input" type="radio" name="flexRadioDefault" id={id} />
+                        <label className="form-check-label" htmlFor='flexRadioDefault1'>
+                            {text}
+                        </label>
+                    </div>
+                )}
+            </form>
+        </div>
     )
 }
  
