0066d12b8cbd34c7397775cd9d89f3d518065173
Semuel <serg_artemenko@ukr.net>
2023-09-12 13:23:14 +0300

    feat: added constants in SortPanel component

--- src/components/sort-filter/SortFilter.jsx
+++ src/components/sort-filter/SortFilter.jsx
@@ -1,6 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { Typography, Box } from '@mui/material';
+
+const titleFilterCost = css`
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 30px;
+    text-transform: uppercase;
+    margin-bottom: 38px;
+`
+
 export const SortFilter = () => {
     return (
-        <h1>Sort component</h1>
-        <Typography variant="h3" css={titleFilterCost}>Вартість:</Typography>
-    );
+        <Box>
+            <Typography variant="h3" css={titleFilterCost}>Сортувати:</Typography>
+
+
+        </Box>
+
+    )
 }
--- src/features/sort-panel/SortPanel.jsx
+++ src/features/sort-panel/SortPanel.jsx
@@ -3,11 +3,25 @@ import { SortFilter } from "../../components";
 import { Typography } from '@mui/material';
 import { css } from '@emotion/react';
 
+
+const SORT_TYPE = [
+    {text: 'По ціні'},
+    {text: 'По типу'},
+    {text: 'По назві'},
+]
+
+const SORT_ICON = [
+    {text: 'Значок зростання'},
+    {text: 'Значок спадання'}
+
+]
+
+
 export const SortPanel = () => {
     return ( 
         <div css={css`margin-bottom: 38px;`}>
             <Typography variant="h3" className="visually-hidden">SortPanel</Typography>
-            <SortFilter />
+            <SortFilter options1={SORT_TYPE} options2={SORT_ICON}/>
         </div>
     );
 }
