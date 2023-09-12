bdd18adec37db069eacd0e2e7aceda5ff6fabf8a
Semuel <serg_artemenko@ukr.net>
2023-09-12 22:30:26 +0300

    feat: delete store.module.css

--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,10 +1,8 @@
 import { FilterPanel, SortPanel } from '../../features'
 
-import styles from './Store.module.css'
-
 export const Store = () => {
     return ( 
-        <div className={styles.wrapper}>
+        <div>
             <div className="container-fluid bg-color-main mb-5 py-5">
                 <h1 className="container text-center">Магазин готовых шаблонов</h1>
             </div>
