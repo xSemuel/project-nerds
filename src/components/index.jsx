8b9642305aa2fd43603b1cc2b7a51c221d549fd3
Semuel <serg_artemenko@ukr.net>
2023-09-12 12:53:34 +0300

    feat: deleted Store component in components folder, which write before

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,7 +2,6 @@ export * from './Header';
 export * from './main';
 export * from './Studio';
 export * from './Clients';
-export * from './Store';
 export * from './Contacts';
 export * from './Cart';
 export * from './Mapcontact';
--- src/features/sort-panel/SortPanel.jsx
+++ src/features/sort-panel/SortPanel.jsx
@@ -2,7 +2,7 @@ import { SortFilter } from "../../components";
 
 export const SortPanel = () => {
     return ( 
-        <div>
+        <div className="">
             <h3>Cортувати:</h3>
             <SortFilter />
         </div>
