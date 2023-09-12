2035273ebfbcf03774ec437caea36b0b901c5215
Semuel <serg_artemenko@ukr.net>
2023-09-12 22:54:35 +0300

    feat: transfer code from sort-panel component to Store component

--- src/features/index.js
+++ src/features/index.js
@@ -1,2 +1,1 @@
 export * from './filter-panel'
-export * from './sort-panel'
--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,6 +1,40 @@
-import { FilterPanel, SortPanel } from '../../features'
+/** @jsxImportSource @emotion/react */
+import { useState } from 'react';
+import { SortFilter } from '../../components';
+import { Typography } from '@mui/material';
+import { css } from '@emotion/react';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { FilterPanel } from '../../features'
+
+const SORT_TYPE = [
+    {text: 'По ціні', value: 'forPrice'},
+    {text: 'По типу', value: 'forType'},
+    {text: 'По назві', value: 'forName'},
+]
+
+const SORT_ICON = [
+    {text: 'Значок зростання', value: 'up', icon: <ArrowDropUpIcon sx={{ color: '#e1e1e1'}} />, checkedIcon: <ArrowDropUpIcon sx={{color: '#d7373b'}} />},
+    {text: 'Значок спадання', value: 'down', icon: <ArrowDropDownIcon sx={{ color: '#e1e1e1'}} />, checkedIcon: <ArrowDropDownIcon sx={{color: '#d7373b'}} /> },
+]
 
 export const Store = () => {
+
+    const [sorts, setSorts] = useState({
+        sortTypeFilter: SORT_TYPE[0].value,
+        sortIconFilter: SORT_ICON[0].value,      
+    })
+
+    const onClickSortTypeHandler = (sortTypeFilter) => {
+        setSorts((prevState) => ({ ...prevState, sortTypeFilter }))
+    }
+
+    const onChangeIconSortHandler = ( sortIconFilter) => {
+        setSorts((prevState) => ({ ...prevState, sortIconFilter }))
+    }
+
+    console.log(sorts)
+
     return ( 
         <div>
             <div className="container-fluid bg-color-main mb-5 py-5">
@@ -13,11 +47,12 @@ export const Store = () => {
                         <FilterPanel />
                     </form>
                     <div className='col-9'>
-                        <SortPanel />
-                        {/* <div css={css`margin-bottom: 38px;`}>
+                        <div css={css`margin-bottom: 38px;`}>
                             <Typography variant="h3" className="visually-hidden">SortPanel</Typography>
                             <SortFilter options1={SORT_TYPE} options2={SORT_ICON} onClick={onClickSortTypeHandler} onChange={onChangeIconSortHandler} selectedSortIcon={SORT_ICON[0].value}/>
-                        </div> */}
+                        </div>
+
+
                         <h1>Карточки товаров</h1>
                     </div>    
                     
