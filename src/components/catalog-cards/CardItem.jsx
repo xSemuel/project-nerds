8ea967aefed6ea304b935830238eada8e6e6dc85
Semuel <serg_artemenko@ukr.net>
2023-09-17 02:01:30 +0300

    feat: added class toggle in CardItem component

--- src/components/catalog-cards/CardItem.jsx
+++ src/components/catalog-cards/CardItem.jsx
@@ -1,32 +1,54 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { Card, CardContent, CardMedia, Typography, Button, CardActionArea, CardActions } from '@mui/material';
+import { useState } from 'react';
 
 export const CardItem = ({options}) => {
     const { title, srcLogo, alt, descInfo, descPrice } = options;
 
+    const [ showCost, setShowCost ] = useState(false)
+    
+    const styledShowCost = (e) => {
+        console.log(e.type)
+        e.type === 'mouseenter' ? setShowCost(true) : setShowCost(false)
+    }
+
+    const itemCost = css `
+    .MuiCardActions {
+        display: absolute;
+        position: fixed;
+        max-width: 360px;
+        max-height: 420px;
+        background-color: #eee;  
+        z-index: -5999;
+        padding-bottom: 300px;
+    }  
+    `
+
     return ( 
         <>
-            <Card key={title} sx={{ maxWidth: 360, maxHeight: 618 }}>
+            <Card key={title} sx={{ maxWidth: 360, maxHeight: 618 }} onMouseEnter={styledShowCost} onMouseLeave={styledShowCost}>
                 <CardActionArea>
                     <CardMedia
                     component="img"
-                    height="518"
+                    height="418"
                     image={srcLogo}
                     alt={alt}
                     />
-                    <CardContent>
+                    {showCost && (<CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         {title}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         {descInfo}
                     </Typography>
-                    </CardContent>
+                    </CardContent>)}
                 </CardActionArea>
-                <CardActions>
+                {showCost && (<CardActions css={itemCost}>
                     <Button size="small" color="primary">
                     {descPrice}
                     </Button>
-                </CardActions>
+                </CardActions>)}
             </Card>
         </>
     )
