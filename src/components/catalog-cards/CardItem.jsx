c80ba3fd39e8c3c7bf6d62075dd6fea61b324fda
Semuel <serg_artemenko@ukr.net>
2023-10-13 18:21:48 +0300

    feat: added Tooltip to CardItem component

--- src/components/catalog-cards/CardItem.jsx
+++ src/components/catalog-cards/CardItem.jsx
@@ -1,56 +1,87 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { Card, CardContent, CardMedia, Typography, Button, CardActionArea, CardActions } from '@mui/material';
-import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import { Card, CardMedia, Typography, Button, CardActionArea, Box } from '@mui/material';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 
 export const CardItem = ({options}) => {
     const { title, srcLogo, alt, descInfo, descPrice } = options;
 
-    const [ showCost, setShowCost ] = useState(false)
-    
-    const styledShowCost = (e) => {
-        console.log(e.type)
-        e.type === 'mouseenter' ? setShowCost(true) : setShowCost(false)
-    }
 
-    const itemCost = css `
-    .MuiCardActions {
-        display: absolute;
-        position: fixed;
-        max-width: 360px;
-        max-height: 420px;
-        background-color: #eee;  
-        z-index: -5999;
-        padding-bottom: 300px;
-    }  
-    `
+    const CardTooltip = styled(({ className, ...props }) => (
+            <Tooltip {...props} classes={{ popper: className }} />
+        ))({
+            [`& .${tooltipClasses.tooltip}`]: {
+                maxWidth: 360,
+                maxHeight: 420,
+                backgroundColor: `#eee`,
+                color: "black",
+            },
+        });
+
+
+        const buttonStyles = css`  
+            width: 160px;
+            height: 40px;
+            margin: 0 auto;
+            background: #eee;
+            color: #000;
+            fontSize: 16px;
+            fontWeight: 500;
+            font-family: inherit;
+            line-height: 18px;
+            text-transform: uppercase;
+            transition: .3s ease-in;
+            &:hover {
+                background: #d7373b;
+                color: #ffffff;
+            }
+            display: flex;
+            gap: 2px;
+        `
+
+        const wrapperDescCardStyle = css`
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            align-items: center;
+            gap: 10px;
+            padding: 5px 0;
+        `
+
+        const currencyButtonStyle = css`
+            font-size: 11px;
+            padding-top: 4px;
+        `
 
     return ( 
-        <>
-            <Card key={title} sx={{ maxWidth: 360, maxHeight: 618 }} onMouseEnter={styledShowCost} onMouseLeave={styledShowCost}>
+            <Card key={title} sx={{ maxWidth: 360, maxHeight: 618 }}>
                 <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="418"
-                    image={srcLogo}
-                    alt={alt}
-                    />
-                    {showCost && (<CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {descInfo}
-                    </Typography>
-                    </CardContent>)}
+                    <CardTooltip title={
+                        <Box css={wrapperDescCardStyle}>
+                            <Typography variant="h5">
+                                {title}
+                            </Typography>
+                            <Typography variant="h6">
+                                {descInfo}
+                            </Typography>
+                            <Button css={buttonStyles}
+                                variant="contained"
+                                size="large">
+                                {descPrice}  
+                                <Typography css={currencyButtonStyle} variant="body2">грн.</Typography>
+                            </Button>
+                        </Box>
+                    }>
+                        <CardMedia
+                        component="img"
+                        height="418"
+                        image={srcLogo}
+                        alt={alt}
+                        />
+                    </CardTooltip>
                 </CardActionArea>
-                {showCost && (<CardActions css={itemCost}>
-                    <Button size="small" color="primary">
-                    {descPrice}
-                    </Button>
-                </CardActions>)}
             </Card>
-        </>
     )
 }
  
