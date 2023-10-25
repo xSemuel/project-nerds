2de28ee045c2b7cd9e82d20ada6a3f17af1acb85
Semuel <serg_artemenko@ukr.net>
2023-10-25 19:28:43 +0300

    refactor: refactored  style in Advantage component

--- src/components/main/Advantage.jsx
+++ src/components/main/Advantage.jsx
@@ -1,22 +1,91 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { Typography, Button, Box, Card } from '@mui/material';
+
 import { NavLink } from "react-router-dom";
 import { ADVANTAGE_BLOCK } from '../../constants';
 
+    const cardStyle = css`
+        width: 18.75rem; 
+        height: 21.87rem;
+    `    
+    const buttonStyles = css`  
+        width: 160px;
+        height: 50px;
+        margin-top: 10px;
+        margin: 0 auto;
+        color: #FFF;
+        text-align: center;
+        font-family: Roboto;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 18px;
+        text-transform: uppercase;
+        transition: .3s ease-in;
+        &:hover {
+            background: #d7373b;
+            color: #ffffff;
+        } 
+    `
+    const imgAdvantageStyle = css`
+        height: 146px; 
+        width: 300px;
+
+    `
+    const advantageTitle = css`
+        color: #000;
+        font-family: Roboto;
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 30px;
+        text-transform: uppercase;
+    `
+
+    const advantageDesc = css`
+        text-align: center;
+        color: #283136;
+        font-family: Roboto;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 24px; 
+    `
+    const wrapperAdvantageInfo = css`
+        padding: 25px 0;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        justify-content: space-beetwen;
+        align-items: center;
+    `
+
+
 export const Advantage = () => {
     return ( 
-        <div className="container mt-5 d-flex justify-content-between">
+        <Box className="container mt-5 d-flex justify-content-between">
             
-            {ADVANTAGE_BLOCK.map(({ title, img, info, alt, btnInfo, btnColor, link }, index) => 
-                <div key={index} className="card" style={{width: '18.75rem', height: '21.87rem'}}>           
-                    <img src={img} className="card-img-top mb-4" alt={alt} height= '146px' width='300px' />
-                    <div className="card-body">
-                        <h3 className="card-title mb-3">{title}</h3>
-                        <p className="card-text mb-4">{info}</p>
-                        <NavLink to={link} className="btn btn-primary" style={{backgroundColor: btnColor}}>{btnInfo}</NavLink>
-                    </div>
-                </div>
+            {ADVANTAGE_BLOCK.map(({ title, img, info, alt, btnInfo, btnColor, link }) => 
+                <Card key={title} css={cardStyle}> 
+                    <Box css={imgAdvantageStyle}
+                        component="img"
+                        src={img}
+                        alt={alt}
+                    />          
+                    <Box css={wrapperAdvantageInfo}>
+                        <Typography css={advantageTitle}>{title}</Typography>
+                        <Typography css={advantageDesc}>{info}</Typography>
+                        <NavLink to={link}>
+                            <Button css={buttonStyles} sx={{backgroundColor: btnColor }}>
+                                {btnInfo}
+                            </Button>
+                        </NavLink>
+                    </Box>
+                </Card>
             )}
 
-        </div>
+        </Box>
 
     )
 }
