f2fe4ee5ce991940f87c8671b38f907feeb1bc1a
Semuel <serg_artemenko@ukr.net>
2023-11-01 22:25:31 +0200

    feat: added app-orderGoodsPage constants for OrderGoodsPage

---
+++ src/constants/app-ordersGoodsPage.jsx
@@ -0,0 +1,55 @@
+
+export const orderCustomer = [
+	{
+		id: "firstName",
+		name: "First name",
+		label: "Ваше ім'я",
+		autoComplete: "given-name",
+	},
+	{
+		id: "lastName",
+		name: "lastName",
+		label: "Ваше прізвище",
+		autoComplete: "family-name",
+	},
+	{
+		id: "email",
+		name: "email",
+		label: "Ваш email",
+		autoComplete: "email",
+	},
+	{
+		id: "telephone",
+		name: "telephone",
+		label: "Ваш номер телефону",
+		autoComplete: "telephone",
+	},
+]	
+
+export const orderDelivery = [
+
+	{
+		id: "state", 
+		name: "state",
+		label: "Область", 
+		autoComplete: "state",
+	},
+	{
+		id: "city",
+		name: "city",
+		label: "Населений пункт",
+		autoComplete: "city",
+	},
+	{
+		id: "numberDepartment",
+		name: "numberDepartment",
+		label: "Номер віділення нової пошти",
+		autoComplete: "numberDepartment",
+	},
+	{
+		id: "adressDepartment",
+		name: "adressDepartment",
+		label: "Адреса віділення нової пошти",						
+		autoComplete: "adressDepartment",
+	},
+]
--- src/features/order-goods/OrderGoodsPage.jsx
+++ src/features/order-goods/OrderGoodsPage.jsx
@@ -7,61 +7,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react'
 
 import { OrderInfoSucess } from './OrderInfoSucess';
+import { orderCustomer, orderDelivery } from '../../constants';
 
-const orderCustomer = [
-	{
-		id: "firstName",
-		name: "First name",
-		label: "Ваше ім'я",
-		autoComplete: "given-name",
-	},
-	{
-		id: "lastName",
-		name: "lastName",
-		label: "Ваше прізвище",
-		autoComplete: "family-name",
-	},
-	{
-		id: "email",
-		name: "email",
-		label: "Ваш email",
-		autoComplete: "email",
-	},
-	{
-		id: "telephone",
-		name: "telephone",
-		label: "Ваш номер телефону",
-		autoComplete: "telephone",
-	},
-]	
-
-const orderDelivery = [
-
-	{
-		id: "state", 
-		name: "state",
-		label: "Область", 
-		autoComplete: "state",
-	},
-	{
-		id: "city",
-		name: "city",
-		label: "Населений пункт",
-		autoComplete: "city",
-	},
-	{
-		id: "numberDepartment",
-		name: "numberDepartment",
-		label: "Номер віділення нової пошти",
-		autoComplete: "numberDepartment",
-	},
-	{
-		id: "adressDepartment",
-		name: "adressDepartment",
-		label: "Адреса віділення нової пошти",						
-		autoComplete: "adressDepartment",
-	},
-]
 
 const buttonStyles = css`  
 	width: 260px;
@@ -135,7 +82,6 @@ export const OrderGoodsPage = () => {
     // const onChangeHandler = (filterName, value) => {
     const onChangeHandler = (e) => {
 		const { id: filterName, value } = e.target
-		console.log(filterName, value)
         setObjectOrderGood((prevState) => ({ ...prevState, [filterName]: value }))
     }
 
