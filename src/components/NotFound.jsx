01ab08007d6012e3d137cf163c0966f5f4beeb98
Semuel <serg_artemenko@ukr.net>
2023-11-02 19:36:27 +0200

    feat: rewrite NotFound component to MUI tags

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { Container, Typography, Box } from '@mui/material';
 import notFound from '../images/404NotFound.png';
+
+    const notFoundContainer = css`
+        margin: 5rem auto;
+        padding: 2px;
+        min-height: 600px;
+    `
+    const notFoundTitle = css`
+        color: #000;
+        font-size: 24px;
+        font-weight: 700;
+        font-family: inherit;
+        line-height: 18px;
+        text-transform: uppercase;
+        text-align: center;
+    `
 
 export const NotFound = () => {
     return ( 
-        <div className='container mt-5'>
-            <h1> Помилка 404. Сторінка на яку Ви посилаєтесь не знайдено!</h1>
-            <div>
-                <img className='' src={notFound} alt="Сторінка не знайдена" />
-            </div>
-        </div>
+        <Container fixed css={notFoundContainer}>
+            <Typography css={notFoundTitle}> 
+                Помилка 404. Сторінка на яку Ви посилаєтесь не знайдено!
+            </Typography>
+            <Box 
+                component='img'
+                src={notFound} 
+                alt="Сторінка не знайдена" 
+            />
+        </Container>
     );
 }
