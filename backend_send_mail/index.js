929a7f5bd8f936930b5da7e57624b160ba15bcb4
Semuel <serg_artemenko@ukr.net>
2023-11-19 15:18:51 +0200

    feat: created backend folder for the sending message to email

---
+++ backend_send_mail/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/send-email', (req, res) => {
+  const { email, message } = req.body;
+
+  // Nodemailer logic to send email
+  // This part involves setting up a transporter, composing an email, and sending it
+
+  // Example transporter setup:
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'your-email@gmail.com',
+      pass: 'your-email-password',
+    },
+  });
+
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to: email,
+    subject: 'Subject of your email',
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent');
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
