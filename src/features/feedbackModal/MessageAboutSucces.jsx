428e206bbe50d786d12b58bbc32332f350957ba5
Semuel <serg_artemenko@ukr.net>
2023-11-26 20:01:41 +0200

    feat: ended styled messageAboutSucces component

--- src/features/feedbackModal/MessageAboutSucces.jsx
+++ src/features/feedbackModal/MessageAboutSucces.jsx
@@ -1,17 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-
 import { Typography } from '@mui/material';
-
 
 const messageStyle = css`
     font-family: Roboto;
-    font-size: 16px;
+    font-size: 24px;
     font-weight: 500;
     line-height: 18px;
     text-transform: uppercase;
+    text-align: center;
+    margin-top: 150px;
 `
-
 
 export const MessageAboutSucces = () => {
     return ( 
